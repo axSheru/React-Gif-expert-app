fd27575d49b1a62d7766e1055503b7c25ba14f51
Alex Paredes <alexparedesr95@gmail.com>
2022-03-04 15:58:41 -0600

    FEAT: Imported GifGrid component.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState([ 'Star Wars', 'Horizon Forbidden West', 'God of War' ]);
+    const [categories, setCategories] = useState([ 'Star Wars' ]);
 
     /* const handleAdd = () => {
         setCategories( cats => [ ...cats, 'Demon Slayer' ] );
@@ -17,9 +18,12 @@ const GifExpertApp = () => {
 
             <ol>
                 {
-                    categories.map( category => {
-                        return <li key={ category }>{ category }</li>;
-                    })
+                    categories.map( category => 
+                        <GifGrid
+                            key={ category }
+                            category={ category }
+                        />
+                    )
                 }
             </ol>
         </>
