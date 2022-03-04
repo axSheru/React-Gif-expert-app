43475fedbb1b4c561a5f4da11fe4aed984a95e44
Alex Paredes <alexparedesr95@gmail.com>
2022-03-04 14:08:03 -0600

    FEAT: Added list of categories and event handler to add a new one on button click.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'Star Wars', 'Horizon Forbidden West', 'God of War' ]);
+
+    const handleAdd = () => {
+        setCategories( cats => [ ...cats, 'Demon Slayer' ] );
+    };
 
     return (
         <>
             <h2>Gif expert app</h2>
             <hr />
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }>{ category }</li>;
+                    })
+                }
+            </ol>
         </>
     );
 
