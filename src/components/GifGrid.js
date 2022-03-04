10fe6bda93a52c104a1561b08f259a3d45b1d19c
Alex Paredes <alexparedesr95@gmail.com>
2022-03-04 16:59:52 -0600

    FEAT: Imported GifGridItem component and sent it the image to be rendered.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
       getGifs();
@@ -21,13 +24,20 @@ export const GifGrid = ({ category }) => {
         });
 
         console.log(gifs);
+        setImages( gifs );
     };
-
-    getGifs();
 
     return (
         <div>
             <h3>{ category }</h3>
+            {
+                images.map( img => ( 
+                        <GifGridItem
+                            key={ img.id }
+                            { ...img }
+                        />
+                ))
+            }
         </div>
     )
 }
