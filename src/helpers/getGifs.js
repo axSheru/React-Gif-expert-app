50c2d323add37501d5bcb7c515a678f8854aa51d
Alex Paredes <alexparedesr95@gmail.com>
2022-03-05 12:15:07 -0600

    REFACTOR: Changed giphy apiKey to be retrieved from .env file.

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=u1775mXvH3OtSHfVaM2EIIfeO4XCxjp9`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ process.env.REACT_APP_GIPHY_API_KEY }`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
