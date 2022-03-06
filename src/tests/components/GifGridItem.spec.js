b98b526627d1c8b05d3883c4bb7b0ff6b9800d88
Alex Paredes <alexparedesr95@gmail.com>
2022-03-05 22:23:26 -0600

    TEST: Updated GifGridItem tests to send title and url props.

--- src/tests/components/GifGridItem.spec.js
+++ src/tests/components/GifGridItem.spec.js
@@ -4,9 +4,11 @@ import { GifGridItem } from "../../components/GifGridItem";
 describe('Pruebas en componente GifGridItem.', () => {
     
     let wrapper;
+    const title = 'Soy un título';
+    const url = 'https://localhost:8080';
 
     beforeEach(() => {
-        wrapper = shallow( <GifGridItem /> );
+        wrapper = shallow( <GifGridItem title={ title } url={ url } /> );
     });
 
     test('debe de hacer match con el snapshot.', () => {
