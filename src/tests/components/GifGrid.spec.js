73df31c67a28c1b5284e29185591c7a521a4a9ee
Alex Paredes <alexparedesr95@gmail.com>
2022-03-06 23:02:06 -0600

    TEST: Added mock userFetchGifs.

--- src/tests/components/GifGrid.spec.js
+++ src/tests/components/GifGrid.spec.js
@@ -1,17 +1,21 @@
 import { shallow } from "enzyme";
 import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+
+jest.mock( '../../hooks/useFetchGifs' );
 
 describe('Pruebas en el componente GifGrid.', () => {
 
-    let wrapper;
     const category = 'Netflix';
-
-    beforeEach(() => {
-        wrapper = shallow( <GifGrid category={ category } /> );
-    });
 
     test('debe de hacer match con el snapshot.', () => {
 
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
+
+        const wrapper = shallow( <GifGrid category={ category } /> );
         expect( wrapper ).toMatchSnapshot();
 
     });
