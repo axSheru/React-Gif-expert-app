c6bd0706afd36704dabebb2830231fd7ae00ad1d
Alex Paredes <alexparedesr95@gmail.com>
2022-03-07 16:00:06 -0600

    TEST: Added test to check the hook state changed correctly.

--- src/tests/hooks/useFetchGifs.spec.js
+++ src/tests/hooks/useFetchGifs.spec.js
@@ -3,13 +3,29 @@ import { useFetchGifs } from "../../hooks/useFetchGifs";
 
 describe('Pruebas en el hook useFetchGifs.', () => {
 
-    test('debe de retornar el estado inicial.', () => {
+    test('debe de retornar el estado inicial.', async() => {
 
-        const { result } = renderHook( () => useFetchGifs( 'Darth Maul' ));
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'Darth Maul' ));
+
+        const { data, loading } = result.current;
+        
+        await waitForNextUpdate();
+
+        expect( data ).toEqual( [] );
+        expect( loading ).toBeTruthy();
+
+    });
+
+    test('debe de retornar un arreglo de imágenes y el loading en false.', async() => {
+
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'Darth Maul' ));
+
+        await waitForNextUpdate();
+
         const { data, loading } = result.current;
 
-        expect( data.length ).toBe( 0 );
-        expect( loading ).toBeTruthy();
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBeFalsy();
 
     });
 
