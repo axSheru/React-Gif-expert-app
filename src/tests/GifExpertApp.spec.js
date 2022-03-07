beef09995d59024d0ce7ec55e2f8235db771ca27
Alex Paredes <alexparedesr95@gmail.com>
2022-03-06 23:35:57 -0600

    TEST: Added test to check the number of GifGrid components matches with the categories sent.

--- src/tests/GifExpertApp.spec.js
+++ src/tests/GifExpertApp.spec.js
@@ -3,15 +3,23 @@ import GifExpertApp from '../GifExpertApp';
 
 describe('Pruebas en el componente GifExpertApp.', () => {
 
-    let wrapper;
-
-    beforeEach( () => {
-        wrapper = shallow( <GifExpertApp /> );
-    });
-
     test('debe de hacer match con el snapshot.', () => {
 
+        const wrapper = shallow( <GifExpertApp /> );
+
         expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('debe de mostrar una lista de categorías.', () => {
+
+        const categories = [ 'The mandalorian', 'Back to the future' ];
+        const wrapper = shallow( <GifExpertApp defaultCategories={ categories } /> );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find( 'GifGrid' ).length ).toBe( categories.length );
+
+
 
     });
 
